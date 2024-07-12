b02609a5f48ccf6e66f0d8973fa33fd8bd17d4b0
Tasif <tasif006@gmail.com>
2024-07-11 22:57:24 -0400

    added key combination remapping

--- keyboard_remap.cpp
+++ keyboard_remap.cpp
@@ -4,6 +4,7 @@
 
 HHOOK hHook;
 std::map<int, int> keymap;
+std::map<std::pair<int, int>, int> keymapcombo;
 bool exitFlag = false;
 
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
@@ -17,6 +18,19 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
           exitFlag = true;
           PostQuitMessage(0);
           return 1;
+        }
+
+        for (auto it = keymapcombo.begin(); it != keymapcombo.end(); ++it) {
+          if (p->vkCode == it->first.first && GetAsyncKeyState(it->first.second) & 0x8000 ||
+              p->vkCode == it->first.second && GetAsyncKeyState(it->first.first) & 0x8000) {
+            INPUT input;
+            input.type = INPUT_KEYBOARD;
+            input.ki.wVk = it->second;
+            input.ki.dwFlags = 0;
+
+            SendInput(1, &input, sizeof(INPUT));
+            return 1;
+          }
         }
 
         if (keymap.find(p->vkCode) != keymap.end())
@@ -37,11 +51,21 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 
 void initKeymap() {
   keymap['A'] = 'B';
+
+  keymapcombo[std::make_pair<int, int>('B', 'C')] = 'D';
 }
 
 int main() {
   initKeymap();
   hHook = SetWindowsHookEx(WH_KEYBOARD_LL, LowLevelKeyboardProc, NULL, 0);
+
+  for (auto it = keymap.begin(); it != keymap.end(); ++it) {
+    printf("Key %c remapped to %c\n", it->first, it->second);
+  }
+
+  for (auto it = keymapcombo.begin(); it != keymapcombo.end(); ++it) {
+    printf("Key combo %c + %c remapped to %c\n", it->first.first, it->first.second, it->second);
+  }
 
   printf("Keyboard remapped, press ESC to exit\n");
   MSG msg;
